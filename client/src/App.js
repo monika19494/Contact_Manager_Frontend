f663d12e56768da11481f8315b08055eb094c589
Bhushan Patil <bhushanp749@gmail.com>
2023-01-04 09:48:39 +0530

    Updated Contact PAge

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,11 @@ import './index.css';
 import { Provider } from './components/axios/axioscontext';
 import Login from"./components/Pages/Login";
 import Register from "./components/Pages/Register";
-// import { Navigate } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ContactPage from './components/ContactPages/ContactPage';
 import Protected_route from './components/Pages/protected_route';
 function App() {
-//   const token=localStorage.getItem("token")
-//   console.log(token)
+
   return (
     <div>
       <BrowserRouter>
